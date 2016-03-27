ac830c5eb06d788016d8a90a3ec2532566767aea
Severin Kaderli <severin.kaderli@gmail.com>
2016-03-27 21:37:29 +0200

    Implemented a fix for shuffling

--- js/player.js
+++ js/player.js
@@ -11,12 +11,10 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 const Player = {
-
     YTPlayer: undefined,
     updateTimeInterval: undefined,
     updateMetaInterval: undefined,
-    activeKeys : {},
-
+    activeKeys: {},
     GUI: {
         gui: document.getElementById("gui"),
         title: document.getElementById("title"),
@@ -30,27 +28,30 @@ const Player = {
         searchField: document.getElementById("searchField"),
         preloader: document.getElementById("preloader")
     },
-
     /**
      * Gets fired when the YT Player is Loaded
      */
     onReady: function(event) {
 
-        //Remove preloader
-        Player.GUI.preloader.style.display = "none";
-        Player.GUI.gui.style.display = "block";
-
-        Player.YTPlayer.setShuffle(true);
         Player.YTPlayer.unMute();
         Player.YTPlayer.setVolume(100);
 
+        // The timeout is needed to set the shuffle correctly this is a bug
+        // in the YouTube iFrame-API.
+        setTimeout(function() {
+            Player.YTPlayer.setShuffle(true);
+            Player.YTPlayer.setLoop(true);
+        }, 2000);
+
+        //Remove preloader
+        Player.GUI.preloader.style.display = "none";
+        Player.GUI.gui.style.display = "block";
         Player.GUI.controls.prev.addEventListener('click', Player.playPrevious);
         Player.GUI.controls.next.addEventListener('click', Player.playNext);
 
         //Add Keyboard Shortcuts
         window.addEventListener("keydown", Player.keydown, false);
         window.addEventListener("keyup", Player.keyup, false);
-        
         Player.updateTimeInterval = setInterval(Player.updateTime, 500);
         Player.updateMetaInterval = setInterval(Player.displayMeta, 500);
 
@@ -67,36 +68,30 @@ const Player = {
     onStateChange: function(event) {
         const state = event.target.getPlayerState();
     },
-
     keydown: function(e) {
-
-        if(Player.activeKeys[e.keyCode] == null) {
-           switch(e.keyCode) {
-            case 37:
-                Player.playPrevious();
-                break;
-            case 39:
-                Player.playNext();
-                break;
-            case 32:
-                Player.changeVideoState();
-                break;
-            } 
-
+        if (Player.activeKeys[e.keyCode] == null) {
+            switch (e.keyCode) {
+                case 37:
+                    Player.playPrevious();
+                    break;
+                case 39:
+                    Player.playNext();
+                    break;
+                case 32:
+                    Player.changeVideoState();
+                    break;
+            }
             Player.activeKeys[e.keyCode] = true;
         }
-        
     },
-
     keyup: function(e) {
         Player.activeKeys[e.keyCode] = null;
     },
-
     /**
      * Switches between playing and pausing
      */
     changeVideoState: function() {
-        if(Player.YTPlayer.getPlayerState() == YT.PlayerState.PLAYING) {
+        if (Player.YTPlayer.getPlayerState() == YT.PlayerState.PLAYING) {
             clearInterval(Player.updateTimeInterval);
             Player.pause();
         } else {
@@ -104,24 +99,19 @@ const Player = {
             Player.play();
         }
     },
-
     displayMeta: function() {
         let title = Player.YTPlayer.getVideoData().title;
         Player.GUI.title.innerText = title;
         document.getElementsByTagName('title')[0].innerText = title;
-
         //Add event listeners for the GUI
         Player.GUI.controls.play.addEventListener("click", Player.changeVideoState);
     },
-
     playNext: function() {
         Player.YTPlayer.nextVideo();
     },
-
     playPrevious: function() {
         Player.YTPlayer.previousVideo();
     },
-
     /**
      * Formats the seconds in the style mm:ss
      * @param {number} seconds
@@ -134,21 +124,18 @@ const Player = {
         let time = Player.str_pad_left(minutes, "0", 2) + ':' + Player.str_pad_left(seconds, "0", 2);
         return time;
     },
-
     /**
      * This function is used to update the time correctly
      * @return {[type]} [description]
      */
     updateTime: function() {
-
         let currentTime = Player.YTPlayer.getCurrentTime();
         Player.GUI.time.innerText = Player.formatTime(currentTime);
         let duration = Player.YTPlayer.getDuration();
         Player.GUI.duration.innerText = Player.formatTime(duration);
         let progress = currentTime / duration;
-        document.getElementById("trackbar").style.width = progress*100 + "%";
+        document.getElementById("trackbar").style.width = progress * 100 + "%";
     },
-
     /**
      * Start/continue playback of video
      */
@@ -156,7 +143,6 @@ const Player = {
         Player.GUI.controls.play.innerText = "pause_circle_filled";
         Player.YTPlayer.playVideo();
     },
-
     /**
      * Pause the playback of the video
      */
@@ -164,24 +150,20 @@ const Player = {
         Player.GUI.controls.play.innerText = "play_circle_filled";
         Player.YTPlayer.pauseVideo();
     },
-
     loadVideoById: function(id) {
         Player.YTPlayer.loadVideoByid(id);
     },
-
     /**
      * Used the pad the time correctly
      */
-    str_pad_left: function(string,pad,length) {
-        return (new Array(length+1).join(pad)+string).slice(-length);
+    str_pad_left: function(string, pad, length) {
+        return (new Array(length + 1).join(pad) + string).slice(-length);
     },
-
     loadByUrl: function(url) {
         console.log("This should get printed to the console...");
         Player.YTPlayer.loadVideoByUrl(url);
         Player.play();
     },
-
     loadVideo: function() {
         Player.clearPlayer();
         Player.YTPlayer = new YT.Player('player', {
@@ -191,39 +173,32 @@ const Player = {
             }
         });
     },
-
     clearPlayer: function() {
-        if(Player.YTPlayer !== undefined && Player.YTPlayer !== null) {
+        if (Player.YTPlayer !== undefined && Player.YTPlayer !== null) {
             Player.pause();
             Player.YTPlayer.stopVideo();
             Player.YTPlayer.clearVideo();
             Player.YTPlayer.destroy();
         }
-
         Player.GUI.gui.style.display = "none";
         Player.GUI.preloader.style.display = "block";
-        
         //Remove Event Listener
         Player.GUI.controls.play.removeEventListener("click", Player.changeVideoState);
-
         //Create new player element
         let body = document.getElementsByTagName("body")[0];
         let tmp = document.getElementById("player");
-        if(tmp !== undefined && tmp !== null) {
+        if (tmp !== undefined && tmp !== null) {
             body.removeChild(document.getElementById("player"));
         }
-
         let iframe = document.createElement("iframe");
         iframe.id = "player";
         iframe.width = "1";
         iframe.height = "1";
-
         //TODO: Get the url from the box and filter out the list parameter
         let playlistId = getParameterByName("list", Player.GUI.searchField.value);
         iframe.src = 'https://www.youtube.com/embed/videoseries?list=' + playlistId + '&enablejsapi=1';
         body.appendChild(iframe);
         //var player '<iframe id="player" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=FLxoPKKKHEIIH-1wB1XFcypw&enablejsapi=1" ></iframe>';
-
         //Clear intervals and the YT.Player object
         clearInterval(Player.updateTimeInterval);
         clearInterval(Player.updateMetaInterval);
