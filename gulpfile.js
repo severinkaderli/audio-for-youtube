33f15755943031666a93e71bfbb79d0f1b1e1505
Severin Kaderli <severin.kaderli@gmail.com>
2016-04-18 19:50:31 +0200

    Moved to SASS for styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require("gulp"),
+	sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
 	babel = require("gulp-babel"),
 	jshint = require("gulp-jshint"),
 	rename = require("gulp-rename"),
@@ -10,9 +12,17 @@ var gulp = require("gulp"),
 gulp.task('js', function () {
     return gulp.src('js/app.js')
         .pipe(babel())
-        .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('js/'));
+});
+
+gulp.task('sass', function() {
+    return gulp.src('css/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css/'));
 });
 
 /**
